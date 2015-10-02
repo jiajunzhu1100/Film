f49062b6cd866aaf25c39b81d68f2c197731eb01
chukong <408601774@qq.com>
2015-10-02 23:29:57 +0800

    test

--- Film/Film/ViewController.m
+++ Film/Film/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    sv=[[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, 400, 600)];
+    sv.contentSize=CGSizeMake(600, 2000);
+    [self.view addSubview:sv];
 }
 
 - (void)didReceiveMemoryWarning {
